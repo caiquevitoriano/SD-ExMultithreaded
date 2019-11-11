55a9a13192d0697e23793a455ec0598d3fc16c85
caiquevitoriano <vitorianocaique@gmail.com>
2019-11-11 12:03:17 -0300

    iniciando

---
+++ src/sd/exmultithreaded/Buffer.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package sd.exmultithreaded;
+
+/**
+ *
+ * @author caique
+ */
+public interface Buffer {
+    
+    public void set(int valor);
+    public int get();
+}
