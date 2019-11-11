21a1cbeb28bf9a3b5dc28c46a223e62d0dfaa161
caiquevitoriano <vitorianocaique@gmail.com>
2019-11-11 13:49:04 -0300

    exemplo sem sincronização

--- src/sd/exmultithreaded/BufferExemploTeste.java
+++ src/sd/exmultithreaded/BufferExemploTeste.java
@@ -5,17 +5,29 @@
  */
 package sd.exmultithreaded;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 /**
  *
  * @author caique
  */
-public class BufferExemploTeste{
+public class BufferExemploTeste {
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        ExecutorService teste = Executors.newFixedThreadPool(2);
+        Buffer localizacaoCompartilhada = new BufferExemplo();
+        try {
+            teste.execute(new Produtor(localizacaoCompartilhada));
+            teste.execute(new Consumidor(localizacaoCompartilhada));
+        } catch (Exception exception) {
+            exception.printStackTrace();
+        }
+        teste.shutdown();
     }
-    
+
 }
